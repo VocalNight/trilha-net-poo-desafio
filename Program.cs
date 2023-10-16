df634bb11b8d4a6bd1816d89ce0fb57e0fcbd1fb
Vocal <vocalnight@protonmail.com>
2023-10-15 23:18:42 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone("99225567", "Modelo 90", "888888", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("League of legends");
+
+Console.WriteLine("\n --------------- \n");
+
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia("33333", "Modelo 2", "111", 255);
+nokia.Ligar();
+nokia.InstalarAplicativo("Dota 2");
